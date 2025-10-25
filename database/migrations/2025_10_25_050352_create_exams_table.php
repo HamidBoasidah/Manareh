b0d5febee6d27d41f215f0f569640d3bbac4645f
HamidBoasidah <140933334+HamidBoasidah@users.noreply.github.com>
2025-10-25 09:37:23 +0300

    Update 2025_10_25_050352_create_exams_table.php

--- database/migrations/2025_10_25_050352_create_exams_table.php
+++ database/migrations/2025_10_25_050352_create_exams_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('exams', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('circle_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('student_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('exam_type_id')->constrained('exam_types')->cascadeOnDelete();
+            $table->date('exam_date_g');
+            $table->string('exam_date_h')->nullable();
+            $table->unsignedTinyInteger('juzz')->nullable(); // 1..30
+            $table->unsignedSmallInteger('total_points')->nullable();
+            $table->string('total_grade')->nullable(); // نسبة/حرف
+            $table->text('remarks')->nullable();
+            $table->timestamps();
+
+            $table->index(['student_id','exam_date_g']);
+            $table->index(['circle_id','exam_type_id']);
             $table->timestamps();
         });
     }
