133a93b02b032a1e3ff3450c5b7bd22cdd2248de
HamidBoasidah <140933334+HamidBoasidah@users.noreply.github.com>
2025-10-25 08:18:03 +0300

    Update 2025_10_25_050351_create_staff_assignments_table.php

--- database/migrations/2025_10_25_050351_create_staff_assignments_table.php
+++ database/migrations/2025_10_25_050351_create_staff_assignments_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('staff_assignments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('circle_id')->constrained()->cascadeOnDelete();
+            $table->enum('role_in_circle', ['teacher','supervisor_edu','supervisor_tarbawi']);
+            $table->date('start_at');
+            $table->date('end_at')->nullable(); // NULL = مستمر
+            $table->text('notes')->nullable();
+
+            $table->unique(['circle_id','user_id','role_in_circle'], 'uniq_circle_user_role');
+            $table->index(['user_id','role_in_circle','end_at']);
+            $table->index(['circle_id','role_in_circle','end_at']);
             $table->timestamps();
         });
     }
