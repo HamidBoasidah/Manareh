5af2950421e28ca8e63f3e8c900ba7d6b05c27b2
HamidBoasidah <140933334+HamidBoasidah@users.noreply.github.com>
2025-10-25 09:08:41 +0300

    Update 2025_10_25_050351_create_terms_table.php

--- database/migrations/2025_10_25_050351_create_terms_table.php
+++ database/migrations/2025_10_25_050351_create_terms_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('terms', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('academic_year_id')->constrained()->cascadeOnDelete();
+            $table->enum('name', ['T1','T2']);
+            $table->date('start_date_g')->nullable();
+            $table->date('end_date_g')->nullable();
+            $table->string('start_date_h')->nullable();
+            $table->string('end_date_h')->nullable();
             $table->timestamps();
         });
     }
