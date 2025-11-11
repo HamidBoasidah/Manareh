6c0bcb9dbc746d39f7f5271a0355848175f76ecd
HamidBoasidah <hamid.boasidah@gmail.com>
2025-11-11 16:33:50 +0300

    Update

--- database/seeders/NotificationSeeder.php
+++ database/seeders/NotificationSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-class NotificationsSeeder extends Seeder
+class NotificationSeeder extends Seeder
 {
     /**
      * Run the database seeds.
