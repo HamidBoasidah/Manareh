0871c6b911246eee89e6dbb372ab028681ee320d
HamidBoasidah <140933334+HamidBoasidah@users.noreply.github.com>
2025-11-11 10:34:45 +0300

    Update NotificationRepository.php

--- app/Repositories/NotificationRepository.php
+++ app/Repositories/NotificationRepository.php
@@ -8,12 +8,21 @@ use App\Repositories\Eloquent\BaseRepository;
 class NotificationRepository extends BaseRepository
 {
     protected array $defaultWith = [
-        'user:id,name',
-        'template:id,code,channel,locale,subject',
+        'template:id,code,subject',
     ];
 
     public function __construct(Notification $model)
     {
         parent::__construct($model);
+    }
+
+    public function unreadForUser(string $type, int $id)
+    {
+        return $this->model
+            ->where('recipient_type', $type)
+            ->where('recipient_id', $id)
+            ->where('status', 'queued')
+            ->latest()
+            ->get();
     }
 }
